0f47b89186797a65d2cd792fdbba8e13d7046d87
rtgher <ghermanrt@outlook.com>
2021-08-24 20:12:59 +0100

    +Connection Interfaces

---
+++ SpaceMUD.Server/Base/Interface/Connection/IConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Base.Interface.Connection
+{
+    /// <summary>
+    /// Server-side object wrapping the client connection.
+    /// </summary>
+    public interface IConnection
+    {
+        IConnection OnConnect();
+        IConnection OnDisconnect();
+        IConnection Disconnect();
+        IConnection OnUpdate();
+
+    }
+}
