0c37bf1c80e69076e50e9d56acfd7e411db5699b
rtgher <ghermanrt@outlook.com>
2021-08-24 19:02:41 +0100

    ~modify logger to add purge for log ~fixed .csproj location

--- SpaceMUD.Common/Logging/Logger.cs
+++ SpaceMUD.Common/Logging/Logger.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SpaceMUD.Common.Interfaces
+using SpaceMUD.Common.Interfaces;
 
 namespace SpaceMUD.Common.Logging
 {
     public class Logger : ILog
     {
         private StringBuilder _logBuffer;
-        private readonly int MaxLineCount;
+        private readonly int MaxCharCount;
         private string time => DateTime.Now.ToString("g");
         private string _logSavePath;
         public StringBuilder LogBuffer => _logBuffer = _logBuffer ?? new StringBuilder();
@@ -18,7 +19,7 @@ namespace SpaceMUD.Common.Logging
         public Logger(string logSavePath, int maxCharCount= 5000)
         {
             _logSavePath = logSavePath;
-            MaxLineCount = maxCharCount;
+            MaxCharCount = maxCharCount;
         }
 
         public void LogError(string message, Exception exception)
@@ -26,7 +27,7 @@ namespace SpaceMUD.Common.Logging
             string log = $"[{time}]-[ERROR]:{message}\r\n[StackTrace]: {exception.StackTrace}";
             Console.WriteLine(log);
             _logBuffer.AppendLine(log);
-            if(_logBuffer.Length>=MaxLineCount)
+            if (_logBuffer.Length >= MaxCharCount) PurgeLog();
         }
 
         public void LogInfo(string info)
@@ -34,6 +35,7 @@ namespace SpaceMUD.Common.Logging
             string log = $"[{time}]-[Info]: {info}";
             Console.WriteLine(log);
             _logBuffer.AppendLine(log);
+            if (_logBuffer.Length >= MaxCharCount) PurgeLog();
         }
 
         public void LogWarning(string warningInfo, Exception caughtException = null)
@@ -41,11 +43,13 @@ namespace SpaceMUD.Common.Logging
             string log = $"[{time}]-[Warning]:{warningInfo}\r\n[StackTrace]: {caughtException?.StackTrace?? "No Stack trace available."}";
             Console.WriteLine(log);
             _logBuffer.AppendLine(log);
+            if (_logBuffer.Length >= MaxCharCount) PurgeLog();
         }
 
         public void PurgeLog()
         {
-            throw new NotImplementedException();
+            using (StreamWriter file = new StreamWriter(_logSavePath + "/gamelog.txt", append: true)) file.WriteLine(LogBuffer.ToString());
+            _logBuffer.Clear();
         }
     }
 }
