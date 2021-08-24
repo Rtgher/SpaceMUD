2a94599965f7a16f06f332d10634314d5a09f0b7
rtgher <ghermanrt@outlook.com>
2021-08-24 20:13:15 +0100

    + Add Directory helper class

---
+++ SpaceMUD.Common/Tools/DirectoryHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Tools
+{
+    /// <summary>
+    /// A helper static class for directory information.
+    /// </summary>
+    public static class DirectoryHelper
+    {
+        public static DirectoryInfo GetInstalationDirectoryRoot()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            DirectoryInfo directory = new DirectoryInfo(location);
+
+            return directory.Parent;
+        }
+    }
+}
