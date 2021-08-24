a7baaa44566667b6cdd87583413d21059a5f54eb
rtgher <ghermanrt@outlook.com>
2021-08-24 20:15:03 +0100

    ~barebone/partial implementation of server

--- SpaceMUD.Server/Server/TelnetSocketServer.cs
+++ SpaceMUD.Server/Server/TelnetSocketServer.cs
@@ -3,39 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Net.Sockets;
 using SpaceMUD.Base.Interface;
 using SpaceMUD.Common.Interfaces;
-using System.Net;
+using SpaceMUD.Base.Interface.Connection;
+using SpaceMUD.Common.Logging;
+using SpaceMUD.Common.Tools;
 
 namespace SpaceMUD.Server
 {
-    public class TelnetSocketServer : IServer
+    public class TelnetSocketServer : IServer, IDisposable
     {
         Socket serverSocket;
+        List<ISocketConnection> Connections;
+        List<IDisposable> Disposables;
+        ILog Log;
 
-        private Socket BuildSocket()
+        public bool Running { get; private set; } = false;
+        public IGame Game{ get; private set;}
+
+        private int _portNumber;
+        private Socket BuildSocket(int portNumber)
         {
             var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            socket.Bind(new IPEndPoint(IPAddress.Any, _portNumber));
+            socket.Bind(new IPEndPoint(IPAddress.Any, portNumber));
             return socket;
         }
         
-        private int _portNumber;
 
-        public TelnetSocketServer(IGame game)
+
+        public TelnetSocketServer(IGame game, int portNumber=4000)
         {
+            _portNumber = portNumber;
+            Log = new Logger(DirectoryHelper.GetInstalationDirectoryRoot().FullName);
 
+            Connections = new List<ISocketConnection>();
+            Disposables = new List<IDisposable>();
+            serverSocket = BuildSocket(_portNumber);
+            Game = game;
+            //TODO: Innitialise game 
+            //TODO: set Running to true when game starts.
+            Disposables.AddRange(new IDisposable[]{ serverSocket, (IDisposable)Log });
+            try
+            {
+                StartServer(_portNumber);
+            }
+            catch (Exception any)
+            {
+                Log.LogError($"Caught exception while trying to start server. \r\n Aborting...", any);
+                this.Dispose();
+                return;
+            }
         }
 
         public void StartServer(int portNumber)
         {
             throw new NotImplementedException();
+            Running = true;
         }
 
         public void Stop()
         {
             throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (Running) Stop();
+            foreach (IDisposable disposable in Disposables) disposable.Dispose();
         }
     }
 }
