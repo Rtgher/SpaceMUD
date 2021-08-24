166d8ea2eecf7f1f9cdd69968f62b5e7ba13ce0c
rtgher <ghermanrt@outlook.com>
2021-08-24 18:25:41 +0100

    +Common Interfaces

---
+++ SpaceMUD/Base/Interface/IServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Base.Interface
+{
+    /// <summary>
+    /// An interface to detail a basic server.
+    /// </summary>
+    interface IServer
+    {
+        /// <summary>
+        /// Start the server for connection on the given port number.
+        /// </summary>
+        /// <param name="portNumber">The port number to connect to.</param>
+        void StartServer(int portNumber);
+
+        /// <summary>
+        /// Stop the server and end connections.
+        /// </summary>
+        void Stop();
+    }
+}
