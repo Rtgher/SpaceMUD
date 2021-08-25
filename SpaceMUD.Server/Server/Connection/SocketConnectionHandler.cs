257497f0c4a8dc68d8c436675cd90c63486d6180
rtgher <ghermanrt@outlook.com>
2021-08-25 11:47:01 +0100

    +Add Socket Connection Handler

---
+++ SpaceMUD.Server/Server/Connection/SocketConnectionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceMUD.Server.Base.Interface.Connection;
+
+namespace SpaceMUD.Server.Connection
+{
+    public class SocketConnectionHandler : ISocketConnection, IDisposable
+    {
+        Socket ISocketConnection.ClientSocket => throw new NotImplementedException();
+
+        IConnection IConnection.Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        IConnection IConnection.OnConnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        IConnection IConnection.OnDisconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        IConnection IConnection.OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
