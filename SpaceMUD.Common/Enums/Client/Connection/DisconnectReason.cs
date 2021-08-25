d6ca791a6bdd4cd70eec65e4a162a20d6dea6666
rtgher <ghermanrt@outlook.com>
2021-08-25 17:22:00 +0100

    +Add specific MUD errors. +add DisconnectReasons

---
+++ SpaceMUD.Common/Enums/Client/Connection/DisconnectReason.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Enums.Client.Connection
+{
+    public enum DisconnectReason
+    {
+        /// <summary>
+        /// Blanked Disconnect Reason for when we have no idea what happened. Also works as Default.
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Disconnect because of player input/loss of connection
+        /// </summary>
+        PlayerDisconnect = 1,
+        /// <summary>
+        /// Disconnect Reason for when the server kicks the client out.
+        /// </summary>
+        ServerKick = 2,
+        /// <summary>
+        /// Disconnect as server is shutting down.
+        /// </summary>
+        ServerClose = 3
+    }
+}
---
+++ SpaceMUD.Common/Exceptions/Server/ServerFlowException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Exceptions.Server
+{
+    public class ServerFlowException : MUDException
+    {
+        public ServerFlowException()
+        {
+        }
+
+        public ServerFlowException(string message) : base(message)
+        {
+        }
+    }
+}
