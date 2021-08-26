aa1189170abe5dfbcfe7ee6c263514ed1d82d14a
rtgher <ghermanrt@outlook.com>
2021-08-26 20:13:28 +0100

    +add directoryinfo extension class

---
+++ SpaceMUD.Common/Tools/Extensions/DirectoryInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Tools.Extensions
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static DirectoryInfo CreateIfNotExist(this DirectoryInfo self)
+        {
+            if (!self.Exists) self.Create();
+            return self;
+        }
+
+        public static FileInfo CreateIfNotExist(this FileInfo self)
+        {
+            if (!self.Exists) self.Create();
+            return self;
+        }
+
+        public static FileInfo GetOrCreateFileByName(this DirectoryInfo self, string name)
+        {
+            return new FileInfo($"{self.FullName}\\{name}").CreateIfNotExist();
+        }
+    }
+}
