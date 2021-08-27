285981fdcd71e76a37482b942d80c7269e18e786
rtgher <ghermanrt@outlook.com>
2021-08-27 23:47:08 +0100

    +Add Database Exception. +Add IDatabaseObject interface to force objects to have an id.

---
+++ SpaceMUD.Common/Exceptions/Database/Base/MUDDatabaseException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Exceptions.Database.Base
+{
+    public class MUDDatabaseException : MUDException
+    {
+        public MUDDatabaseException()
+        {
+        }
+
+        public MUDDatabaseException(string message) : base(message)
+        {
+        }
+
+        public MUDDatabaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MUDDatabaseException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ SpaceMUD.Common/Exceptions/Database/MUDDatabaseNoDataException.cs
@@ -0,0 +1,29 @@
+﻿using SpaceMUD.Common.Exceptions.Database.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SpaceMUD.Common.Exceptions.Database
+{
+    public class MUDDatabaseNoDataException : MUDDatabaseException
+    {
+        public MUDDatabaseNoDataException()
+        {
+        }
+
+        public MUDDatabaseNoDataException(string message) : base(message)
+        {
+        }
+
+        public MUDDatabaseNoDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MUDDatabaseNoDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
