6662b31d0cac693ec4c1b37d61a3765be22a6903
rtgher <ghermanrt@outlook.com>
2021-08-28 16:05:28 +0100

    ~changes to tests+Add mroe tests

---
+++ Tests/SpaceMUD.Database.Tests/Repositor/FileRepository/FileRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpaceMUD.Database.Repositor.Base.Interface;
+using SpaceMUD.Database.Repositor.FileRepository;
+using SpaceMUD.Data.Base.Interface;
+using System.IO;
+using System.Linq;
+
+namespace SpaceMUD.Database.Tests.Repositor.FileRepository
+{
+    [TestClass]
+    public class FileRepositoryTests
+    {
+        class MockGameObject : IDatabaseObject { }
+        [TestMethod]
+        public void TestGetAll_DoesntError()
+        {
+            IRepository<MockGameObject> repo = new FileRepository<MockGameObject>();
+            var objects = repo.GetAll();
+            Assert.IsNotNull(objects);
+        }
+
+        [TestMethod]
+        public void TestFileRepository_CreatesFolders()
+        {
+            IRepository<MockGameObject> repo = new FileRepository<MockGameObject>();
+            repo.GetAll();
+            DirectoryInfo path = ((FileRepository<MockGameObject>)repo).RootDirectory;
+
+            Assert.IsFalse(path.GetDirectories()?.Count() == 0);
+            Assert.IsNotNull(path.GetDirectories("MockGameObject").First());
+        }
+
+        [TestMethod]
+        public void TestFileRepository_InsertCreatesFile()
+        {
+            IRepository<MockGameObject> repo = new FileRepository<MockGameObject>();
+            var obj = new MockGameObject();
+            obj = repo.Insert(obj);
+
+            Assert.IsTrue(((FileRepository<MockGameObject>)repo).RootDirectory.GetDirectories("MockGameObject").First().GetFiles($"{obj.Id}.json").Count()==1);
+        }
+
+        [TestMethod]
+        public void TestFileRepository_GetAllReturnsallFiles()
+        {
+            IRepository<MockGameObject> repo = new FileRepository<MockGameObject>();
+            int countOfFiles = ((FileRepository<MockGameObject>)repo).RootDirectory.GetDirectories("MockGameObject").First().GetFiles().Count();
+
+            var allMockObj = repo.GetAll();
+
+            Assert.AreEqual(countOfFiles, allMockObj.Count());
+        }
+    }
+}
