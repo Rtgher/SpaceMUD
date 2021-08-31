28a64b3753b14173e0725aad21f3d64a2578634c
rtgher <ghermanrt@outlook.com>
2021-08-31 21:55:19 +0100

    +Add measurement units enum

---
+++ SpaceMUD.Common/Enums/Data/Measurements/MeasurementUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Enums.Data.Measurements
+{
+    public enum  MeasurementUnit
+    {
+        Metric = 0,
+        Imperial = 1 
+    }
+}
