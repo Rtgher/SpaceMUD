d19a37a79987b33428b758e6b85e946a821dfc59
rtgher <ghermanrt@outlook.com>
2021-08-31 21:56:36 +0100

    +Add empty functional traits

---
+++ SpaceMUD.Entities/Trait/Functional/Base/ICan/ICanDie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Entities.Trait.Functional.Base.ICan
+{
+    public interface ICanDie
+    {
+    }
+}
