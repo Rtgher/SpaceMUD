1a3b86629f082b733a6ae66a0dd8d8c3bff6aa48
rtgher <ghermanrt@outlook.com>
2021-09-01 19:54:12 +0100

    +Add Encryption helper from outside source +Add custom exception for it.

---
+++ SpaceMUD.Common/Exceptions/Server/Account/PasswordEncryptionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Exceptions.Server.Account
+{
+    public class PasswordEncryptionException : MUDException
+    {
+        public PasswordEncryptionException()
+        {
+        }
+
+        public PasswordEncryptionException(string message) : base(message)
+        {
+        }
+
+        public PasswordEncryptionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected PasswordEncryptionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
