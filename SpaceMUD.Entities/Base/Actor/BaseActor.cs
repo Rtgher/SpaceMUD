4bee0a569e93cc158265205624349aff5bac4546
rtgher <ghermanrt@outlook.com>
2021-09-01 19:21:02 +0100

    Changes to BaseActor and BaseCombatant. Mostly implementing more interfaces as the interface tree starts to develop.

--- SpaceMUD.Entities/Base/Actor/BaseActor.cs
+++ SpaceMUD.Entities/Base/Actor/BaseActor.cs
@@ -1,14 +1,17 @@
-﻿using SpaceMUD.Entities.Trait.Descriptive.Base.ICan;
-using SpaceMUD.Entities.Trait.Descriptive.Base.IDo;
+﻿using SpaceMUD.Entities.Trait.Descriptive.Base.IDo;
+using SpaceMUD.Entities.Trait.Descriptive.Base.IHave;
 using SpaceMUD.Entities.Trait.Functional.Base.ICan;
+using SpaceMUD.Entities.Trait.Functional.Base.IDo;
 
 namespace SpaceMUD.Entities.Base.Actor
 {
-    public class BaseActor : GameObject, IHaveHeight, ICanBePickedUp
+    public class BaseActor : GameObject, IHaveHeight, ICanBePickedUpIfImDead
     {
         public IHeight Height { get; private set; }
         public IWeight Weight { get; private set; }
+        public IDoDie DoDie { get; private set; }
+        public IDoBePickedUp DoCanBePickedUp { get; private set; }
 
-        public BaseActor(IHeight height, IWeight weight) { Height = height; Weight = weight; }
+        public BaseActor(IDoDie die, IHeight height, IWeight weight) { DoDie = die; Height = height; Weight = weight; }
     }
 }
--- SpaceMUD.Entities/Base/Actor/BaseCombatant.cs
+++ SpaceMUD.Entities/Base/Actor/BaseCombatant.cs
@@ -1,18 +1,19 @@
 ﻿using SpaceMUD.Entities.Trait.Functional.Base.ICan;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SpaceMUD.Entities.Trait.Descriptive.Base.IDo;
+using SpaceMUD.Entities.Trait.Functional.Base.IDo;
+using System.Collections.Generic;
 
 namespace SpaceMUD.Entities.Base.Actor
 {
-    public class BaseCombatant : BaseActor, ICanFight, ICanDie
+    public class BaseCombatant : BaseActor, ICanFight, ICanBeVulnerable
     {
-        public BaseCombatant(ICanFight fight, ICanDie die, IHeight height, IWeight weight) : base(height, weight)
+        public BaseCombatant(IDoFight fight, IDoDie die, IHeight height, IWeight weight) : base(die, height, weight)
         {
             //TODO: Assign and create units.
         }
+
+        public IDoFight DoFight { get; private set; }
+
+        public IList<IDoVulnerable> Vulnerabilities { get; private set; } = new List<IDoVulnerable>();
     }
 }
