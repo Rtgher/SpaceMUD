5496971027e3bde6ac2e4791e6117dc7abd87ab2
rtgher <ghermanrt@outlook.com>
2021-09-03 18:09:46 +0100

    +Add WordNode/treenode interface

---
+++ SpaceMUD.CommandParser/TreeParser/Node/INode.cs
@@ -0,0 +1,23 @@
+﻿using SpaceMUD.CommandParser.TreeParser.Words;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.CommandParser.TreeParser.Node
+{
+    internal interface INode
+    {
+        Word Value
+        {
+            get;
+        }
+        WordNode Parent { get; }
+        WordNode Left { get; }
+        WordNode Right { get; }
+        //Add a new Node
+        WordNode AddNode(WordNode node);
+
+    }
+}
