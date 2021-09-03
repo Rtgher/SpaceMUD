5fb1753cc90692c6bad31c63fa137a8081a228be
rtgher <ghermanrt@outlook.com>
2021-09-03 15:03:32 +0100

    +Add basic race implementation.

---
+++ SpaceMUD.Entities/Trait/Descriptive/Base/IHave/IHaveRace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceMUD.Entities.Trait.Descriptive.Races;
+
+namespace SpaceMUD.Entities.Trait.Descriptive.Base.IHave
+{
+    public interface IHaveRace
+    {
+        Race Race { get; }
+    }
+}
