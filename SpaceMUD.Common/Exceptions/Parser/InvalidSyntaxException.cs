d040eaf5ace731220ddd1b98cb5b9415d1f4d8cb
rtgher <ghermanrt@outlook.com>
2021-09-03 18:09:28 +0100

    +Add Parser Exception

---
+++ SpaceMUD.Common/Exceptions/Parser/InvalidSyntaxException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Exceptions.Parser
+{
+    public class InvalidSyntaxException : MUDException
+    {
+        public InvalidSyntaxException()
+        {
+        }
+
+        public InvalidSyntaxException(string message) : base(message)
+        {
+        }
+
+        public InvalidSyntaxException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidSyntaxException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
