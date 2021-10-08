cedba8ce12e138a8eb6c3d96141cd4497ff2db47
rtgher <ghermanrt@outlook.com>
2021-10-08 22:29:21 +0100

    +Add the messagereceived args for the message received arguments from client

---
+++ SpaceMUD.Server/Server/Connection/Events/MessageReceivedArgs.cs
@@ -0,0 +1,13 @@
+﻿using SpaceMUD.Entities.Network;
+using System;
+
+
+namespace SpaceMUD.Server.Connection.Events
+{
+    public class MessageReceivedArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public Byte[] RawData { get; set; }
+        public Account Account { get; set; }
+    }
+}
