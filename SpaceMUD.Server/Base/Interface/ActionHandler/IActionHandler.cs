478a0ca768a28d0981f3185b68afceccf0ce8ea8
rtgher <ghermanrt@outlook.com>
2021-10-22 01:04:48 +0100

    +Add interface for action handlers.

---
+++ SpaceMUD.Server/Base/Interface/ActionHandler/IActionHandler.cs
@@ -0,0 +1,13 @@
+﻿using SpaceMUD.Server.Base.Interface.Connection;
+using SpaceMUD.Server.Connection;
+using SpaceMUD.Server.Connection.Events;
+
+
+namespace SpaceMUD.Base.Interface.ActionHandler
+{
+    public interface IActionHandler
+    {
+        bool IsDataComplete { get; }
+        void HandleAction(IConnection conn, MessageReceivedArgs args);
+    }
+}
