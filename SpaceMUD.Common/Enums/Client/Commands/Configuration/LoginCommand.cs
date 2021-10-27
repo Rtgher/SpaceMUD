5744bcf3ff66fb754871fd7f024f5eceb397b886
rtgher <ghermanrt@outlook.com>
2021-10-27 16:04:02 +0100

    +Add verb attribute to login

--- SpaceMUD.Common/Enums/Client/Commands/Configuration/LoginCommand.cs
+++ SpaceMUD.Common/Enums/Client/Commands/Configuration/LoginCommand.cs
@@ -5,9 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using SpaceMUD.Common.Commands.Base;
 using SpaceMUD.Common.Enums.Client.CommandData;
+using SpaceMUD.Common.Enums.Data.Functional;
+using SpaceMUD.Common.Tools.Attributes.Parser;
 
 namespace SpaceMUD.Common.Enums.Client.Commands.Configuration
 {
+    [Verb(TargetType.Default, "log in", "login")]
     public class LoginCommand : ICommand
     {
         public CommandsType Type { get; } = CommandsType.Configuration;
