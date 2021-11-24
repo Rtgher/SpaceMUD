1149d1f565b44dcebc34cc1a9fc929429cc10777
rtgher <ghermanrt@outlook.com>
2021-11-24 20:32:19 +0000

    ~cleanup and movement

--- SpaceMUD.Common/Tools/Extensions/AttributeExtensions.cs
+++ SpaceMUD.Common/Tools/Extensions/AttributeExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using SpaceMUD.Common.Tools.Attributes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SpaceMUD.Common.Tools.Extensions
 {
