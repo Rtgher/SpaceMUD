6b9588630df4c5430fab7004fc113cfda6a77cd2
rtgher <ghermanrt@outlook.com>
2021-12-14 17:24:25 +0000

    ~some formating

--- SpaceMUD.CommandParser/TreeParser/TreeParser.cs
+++ SpaceMUD.CommandParser/TreeParser/TreeParser.cs
@@ -34,66 +34,73 @@ namespace SpaceMUD.CommandParser.TreeParser
             for(int i=0; i<verbs.Count(); i++)
             {
                 ICommand buildingCommand = null;
-                Type commandType = null;
                 var verb = verbs.ElementAt(i);
 
-                foreach (var commType in CommandsTypelList)
-                {
-                    if (commType.GetAttribute<PartOfSpeechAttribute>().Synonyms.Contains(verb.Value.Value))
-                    {
-                        commandType = commType;
-                        break;
-                    }
-                }
-                buildingCommand = (ICommand)Activator.CreateInstance(commandType);
-
-                bool reachedNode = false;
-                foreach (var leaf in tree.ParseTree())
-                {
-                    int countUnknown = 0;
-                    if (leaf == verb)
-                    {
-                        reachedNode = true;
-                        continue;
-                    }
-                    if (!reachedNode) continue;//we only want to check the nodes in between the current verb node and the next verb node.
-                    if (leaf.Value.PartOfSpeechType == WordTypeEnum.Verb) break;
-                    if(leaf.Value.PartOfSpeechType == WordTypeEnum.Adverb)
-                    {
-                        buildingCommand.RawData.AdverbValues.Add(leaf.Value.Value);
-                    }
-                    if (leaf.Value.PartOfSpeechType == WordTypeEnum.Equalizer)
-                    {
-                        buildingCommand.RawData.Values.Add(leaf.Left.Value.Value, leaf.Right.Value.Value);
-                        tree.GetEnumerator().MoveNext();
-                        tree.GetEnumerator().MoveNext();//skip over the two added values.
-                    }
-                    if(leaf.Value.PartOfSpeechType == WordTypeEnum.Adjective)
-                    {
-                        buildingCommand.RawData.Values.Add((countUnknown++).ToString(), leaf.Value.Value);
-                    }
-                    if(leaf.Value.PartOfSpeechType == WordTypeEnum.Noun)
-                    {
-                        if(leaf.Left!=null && leaf.Left.Value.PartOfSpeechType == WordTypeEnum.Adjective)
-                        {
-                            buildingCommand.RawData.Values.Add((countUnknown++).ToString(), leaf.Left.Value.Value + " " + leaf.Value.Value);
-                            tree.GetEnumerator().MoveNext();
-                        }
-                    }
-                    if (leaf.Value.PartOfSpeechType == WordTypeEnum.Preposition) continue;
-
-                }
+                BuildCommand(tree, verb, buildingCommand);
 
                 if (parsedCommand == null) parsedCommand = buildingCommand;
                 else parsedCommand.AddFollowUpCommand(buildingCommand);
             }
 
-            
+
             return parsedCommand;
+        }
+
+        private void BuildCommand(IWordTree tree, Node.INode verb, ICommand buildingCommand)
+        {
+            Type commandType = null;
+
+            foreach (var commType in CommandsTypelList)
+            {
+                if (commType.GetAttribute<PartOfSpeechAttribute>().Synonyms.Contains(verb.Value.Value))
+                {
+                    commandType = commType;
+                    break;
+                }
+            }
+
+            buildingCommand = (ICommand)Activator.CreateInstance(commandType);
+
+            bool reachedNode = false;
+            foreach (var leaf in tree.ParseTree())
+            {
+                int countUnknown = 0;
+                if (leaf == verb)
+                {
+                    reachedNode = true;
+                    continue;
+                }
+                if (!reachedNode) continue;//we only want to check the nodes in between the current verb node and the next verb node.
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Verb) break;
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Adverb)
+                {
+                    buildingCommand.RawData.AdverbValues.Add(leaf.Value.Value);
+                }
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Equalizer)
+                {
+                    buildingCommand.RawData.Values.Add(leaf.Left.Value.Value, leaf.Right.Value.Value);
+                    tree.GetEnumerator().MoveNext();
+                    tree.GetEnumerator().MoveNext();//skip over the two added values.
+                }
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Adjective)
+                {
+                    buildingCommand.RawData.Values.Add((countUnknown++).ToString(), leaf.Value.Value);
+                }
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Noun)
+                {
+                    if (leaf.Left != null && leaf.Left.Value.PartOfSpeechType == WordTypeEnum.Adjective)
+                    {
+                        buildingCommand.RawData.Values.Add((countUnknown++).ToString(), leaf.Left.Value.Value + " " + leaf.Value.Value);
+                        tree.GetEnumerator().MoveNext();
+                    }
+                }
+                if (leaf.Value.PartOfSpeechType == WordTypeEnum.Preposition) continue;
+            }
         }
 
         private IWordTree ParseTree(string command)
         {
+
             IWordTree tree =  Dependency.DependencyContainer.Provider.GetService(typeof(IWordTree)) as IWordTree;
             var words = command.Split();
             for (int i = 0; i < words.Length; i++)
