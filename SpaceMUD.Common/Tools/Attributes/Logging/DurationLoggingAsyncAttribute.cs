9503c285eb35f108c2f295f92fb281440b687c49
rtgher <ghermanrt@outlook.com>
2021-12-29 13:55:38 +0000

    +Add usefull logging attributes, and packages.

---
+++ SpaceMUD.Common/Tools/Attributes/Logging/DurationLoggingAsyncAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SpaceMUD.Common.Tools.Attributes.Logging
+{
+    public class DurationLoggingAsyncAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var sw = Stopwatch.StartNew();
+            await next();
+            sw.Stop();
+            Console.WriteLine($"[Duration Info]Method {context.ActionDescriptor.DisplayName} executed in {sw.ElapsedMilliseconds}ms.");
+        }
+    }
+}
---
+++ SpaceMUD.Common/Tools/Attributes/Logging/DurationLoggingAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace SpaceMUD.Common.Tools.Attributes.Logging
+{
+    public class DurationLoggingAttribute : Attribute, IActionFilter
+    {
+        private Stopwatch stopWatch;
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            stopWatch.Stop();
+            Console.WriteLine($"[Duration Info] Method {filterContext.ActionDescriptor.ActionName} took {stopWatch.ElapsedMilliseconds}ms.");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            stopWatch = Stopwatch.StartNew();
+        }
+    }
+}
