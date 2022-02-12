473f6bfa59b3be94ea3896b81c161d2f196cd12a
rtgher <ghermanrt@outlook.com>
2022-02-12 15:26:47 +0000

    +Add GameDriver project.

---
+++ src/MUS.GameDriver/Game.cs
@@ -0,0 +1,13 @@
+﻿using MUS.Common.Interfaces;
+using System.Collections.Generic;
+
+namespace MUS.GameDriver
+{
+    public class Game : IGame
+    {
+        public IList<long> LoggedCharacterIds = new List<long>();
+        public IList<long> LoadedlocationIds = new List<long>();
+
+
+    }
+}
