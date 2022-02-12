d89953fed055af5a2e4855c8a9c72623d7dbbd31
rtgher <ghermanrt@outlook.com>
2022-02-12 16:53:42 +0000

    ~rename exceptions

---
+++ src/MUS.Common/Exceptions/Database/Base/MUSDatabaseException.cs
@@ -0,0 +1,29 @@
+﻿using MUS.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUS.Common.Exceptions.Database.Base
+{
+    public class MUSDatabaseException : MUDException
+    {
+        public MUSDatabaseException()
+        {
+        }
+
+        public MUSDatabaseException(string message) : base(message)
+        {
+        }
+
+        public MUSDatabaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MUSDatabaseException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/MUS.Common/Exceptions/Database/MUSDatabaseNoDataException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using MUS.Common.Exceptions.Database.Base;
+
+namespace MUS.Common.Exceptions.Database
+{
+    public class MUSDatabaseNoDataException : MUSDatabaseException
+    {
+        public MUSDatabaseNoDataException()
+        {
+        }
+
+        public MUSDatabaseNoDataException(string message) : base(message)
+        {
+        }
+
+        public MUSDatabaseNoDataException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MUSDatabaseNoDataException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
